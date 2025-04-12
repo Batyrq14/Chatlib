e3dc45f428e052003d8ccc3dbfcf2bc519ba081b
BatyrCode <utegenov1403@gmail.com>
2025-04-13 03:53:49 +0500

    up to date 2

--- chatlib/build.gradle.kts
+++ chatlib/build.gradle.kts
@@ -41,7 +41,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github"
             artifactId = "chatlib"
-            version = "1.0.4"
+            version = "1.0.5"
             afterEvaluate { from(components["release"]) }
         }
     }
